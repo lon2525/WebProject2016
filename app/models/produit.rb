1d60615d65b95c84a0136c9b3fd98ce12434a257
lon2525 <blastter.lan@gmail.com>
2016-12-20 06:13:48 +0100

    heroku 1er

--- app/models/produit.rb
+++ app/models/produit.rb
@@ -1,7 +1,7 @@
 class Produit < ActiveRecord::Base
 
 	belongs_to :category #2eme type d'association
-	belongs_to :vendeur
+	belongs_to :vendeur #2eme type d'association
 	validates :description, :nom, :prix, :presence => true
 	validates :prix, :numericality => true
 end
